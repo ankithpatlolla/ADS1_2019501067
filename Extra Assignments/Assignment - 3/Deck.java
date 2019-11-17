f349efe429b78afdada08e16256fd7e93fd043c0
ankithpatlolla <ankithpatlolla96@msitprogram.net>
2019-11-17 15:57:33 +0530

    done

--- Extra Assignments/Assignment - 3/Deck.java
+++ Extra Assignments/Assignment - 3/Deck.java
@@ -1,13 +1,28 @@
 import java.util.*;
+/**
+ * class deck to create a playing card with facevalue and suitvalue.
+ * @author Ankith.
+ */
 class Deck implements Comparable<Deck> {
 	int faceValue;
 	int suitValue;
 
+	/**
+	 * constructor for deck class.
+	 * @param suitValue the suitvalue of the card.
+	 * @param faceValue the facevalue of the card.
+	 */
 	Deck (int suitValue, int faceValue) {
 		this.suitValue = suitValue;
 		this.faceValue = faceValue;
 	}
 
+	/**
+	 * compare to method to compare the deck objects to sort.
+	 * @param that the deck which is to be consider.
+	 * @return integer 1, 0, -1.
+	 * Constant complexity.
+	 */
 	public int compareTo (Deck that) {
 		if (this.suitValue > that.suitValue) return 1;
 		else if (this.suitValue < that.suitValue) return -1;
@@ -16,7 +31,9 @@ class Deck implements Comparable<Deck> {
 			return -1;
 		}
 	}
-
+	/**
+	 * toString method to dispaly the output.
+	 */
 	public String toString () {
 		String sval = "";
 		String fval = "";
--- Extra Assignments/Assignment - 3/Solution.java
+++ Extra Assignments/Assignment - 3/Solution.java
@@ -1,6 +1,17 @@
 import java.util.*;
+/**
+ * class solution. main class of my program.
+ * @author Ankith.
+ */
 public class Solution {
 
+	/**
+	 * sort method to sort the deck objects in an array in given order.
+	 * i used insertion sort to sort the array.
+	 * @param arr the array that is to be sorted.
+	 * @return sorted array.
+	 * complexity is O(N ^ 2).
+	 */
 	public Deck[] sort(Deck[] arr) {
 			for (int i = 1; i < arr.length; i++) {
 				for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
@@ -12,6 +23,10 @@ public class Solution {
 		return arr;
 	}
 
+	/**
+	 * method main.my program execution will start from here.
+	 * @param args args.
+	 */
 	public static void main(String[] args) {
 		int[] sval = {1, 2, 3, 4};
 		int[] fval = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
